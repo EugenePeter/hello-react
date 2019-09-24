cd6198e95d92d7e376d4304aed766c83902c8b6a
Eugene <eugenedatsmee@gmail.com>
2019-09-24 21:43:30 +1200

    Display Name at button click

--- public/app.jsx
+++ public/app.jsx
@@ -1,23 +1,50 @@
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {
-            name: 'poy'
+            name: 'poy',
+            message: 'this a default message'
         };
     },
+    getInitialState: function() {
+        return {
+            name: this.props.name
+        }
+    },
+    onButtonClick: function (e) {
+        e.preventDefault();
+
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+            nameRef.value= '';
+
+            if(typeof name === 'string' && name.length > 0) {
+               alert(name);
+
+                this.setState({
+                name: name 
+                });
+            }
+    },
     render: function() {
-        var name = this.props.name;
-        return(
-            <div>
-                <h1> hello my {name} </h1>
-                <p> i love you my poy </p>
+        var name = this.state.name;
+        var message = this.props.message;
+        return ( 
+            <div >
+                <h1> hello there {name} </h1> 
+                <p>{message + '!!'}</p> 
+                
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set name Print to the Screen</button>
+                </form>
             </div>
         );
     }
 });
 
-var lablabnako = 'poy';
+var firstName = 'poy';
 
 ReactDOM.render( 
-    <Greeter name={lablabnako}/>,
+    <Greeter name = {firstName} message="Message from prop"/>,
     document.getElementById('app')
 );
